80751fd23cf315e6456ddb32fabe0d271aed2cd8
Karim Jordan <jordan@hibiscus.coffee>
2023-05-02 16:26:00 +0300

    update `playwright.config.js`

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,37 @@
+import { defineConfig, devices } from '@playwright/test';
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	webServer: {
 		command: 'pnpm build && pnpm preview',
 		port: 4173
 	},
-	testDir: 'tests'
+	testDir: 'tests',
+	reporter: 'html',
+	projects: [
+		/* Test against desktop browsers */
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		},
+		/* Test against mobile viewports. */
+		{
+			name: 'Mobile Chrome',
+			use: { ...devices['Pixel 5'] }
+		},
+		{
+			name: 'Mobile Safari',
+			use: { ...devices['iPhone 12'] }
+		}
+	]
 };
 
-export default config;
+export default defineConfig(config);
